81593ed8b39fa5751e63f0931707466829d76326
Oleg Antonyák <oleg.antonyak@tatum.io>
2022-03-23 14:52:57 +0100

    Chore 2

--- src/components/baseComponents/cards/CodeSnippetCard.tsx
+++ src/components/baseComponents/cards/CodeSnippetCard.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useBuddy } from '../../../providers/Buddy';
 import { BuddyBuilderType } from '../../../types';
 import { CodeSnippet } from '../CodeSnippet';
 import { CodeSnippetEditSection } from './CodeSnippetEditCard';
